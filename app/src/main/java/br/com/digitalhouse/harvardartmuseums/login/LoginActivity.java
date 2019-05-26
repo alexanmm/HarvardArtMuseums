63e77c50c329ecccc531338b5093a0ef2baaa915
alexanmm <alexandre.moreira.medeiros@gmail.com>
2019-05-26 14:20:27 -0300

    subpacotes e criacao de strings

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/login/LoginActivity.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/login/LoginActivity.java
@@ -3,6 +3,8 @@ package br.com.digitalhouse.harvardartmuseums.login;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.digitalhouse.harvardartmuseums.R;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
