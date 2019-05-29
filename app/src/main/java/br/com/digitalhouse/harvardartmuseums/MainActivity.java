c98a2d2a683e29c50cc62588911193f77d0f99c6
Mrodrigues123 <murillozba@hotmail.com>
2019-05-29 13:52:03 -0300

    Desenvolvimento inicial da tela de cadastro

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/MainActivity.java
@@ -1,13 +1,46 @@
 package br.com.digitalhouse.harvardartmuseums;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import br.com.digitalhouse.harvardartmuseums.login.CadastroLoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonCreateaccount = (Button) findViewById(R.id.buttonCreateAccount);
+            buttonCreateaccount.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this, CadastroLoginActivity.class));
+                }
+            });
+
+            Button btnLogin =(Button) findViewById(R.id.BtnLogin);
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+
+
+
+            });
     }
-}
+
+    }
+
+
+
