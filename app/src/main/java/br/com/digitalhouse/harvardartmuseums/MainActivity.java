d0a92e76b1c17243eab523331fd9c9eb4b70035f
Gabriel <gatecos@gmail.com>
2019-06-09 20:47:29 -0300

    Terceiro tratamento Settings

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.digitalhouse.harvardartmuseums;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+   }
