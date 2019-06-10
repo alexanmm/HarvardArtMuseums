51c6a5ec82cb1b4b203319c26a7ef9cd46afe1d7
gustavoshitara <gus_port@yahoo.com.br>
2019-06-10 19:04:50 -0300

    Upload das Telas de Game e Help

---
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/game/GamePlay2Activity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.harvardartmuseums.game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.harvardartmuseums.R;
+
+public class GamePlay2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_play2);
+    }
+}
---
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/help/HelpActivity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.harvardartmuseums.help;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.harvardartmuseums.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
