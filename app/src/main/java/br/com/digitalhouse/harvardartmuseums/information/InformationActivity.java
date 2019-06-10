e2f4bca0f98520e99be72a392e50665aeba465e2
Gabriel <gatecos@gmail.com>
2019-06-09 21:55:25 -0300

    Finalização do Settings e Information

---
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/information/InformationActivity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.harvardartmuseums.information;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.harvardartmuseums.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
