12675a5266b5d78a5df9b9348b8dc28896b2701d
Gabriel <gatecos@gmail.com>
2019-06-09 21:55:25 -0300

    Finalização do Settings e Information

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/home/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/home/MainActivity.java
@@ -1,8 +1,9 @@
-package br.com.digitalhouse.harvardartmuseums;
+package br.com.digitalhouse.harvardartmuseums.home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.digitalhouse.harvardartmuseums.R;
 
 
 public class MainActivity extends AppCompatActivity {
