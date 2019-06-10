e0b31227e5742695ab95b2528465ff4ae077e445
percivalli <khaos36502838>
2019-06-10 19:38:50 -0300

    Alterações Sala.

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/events/EventsActivity.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/events/EventsActivity.java
@@ -1,18 +1,45 @@
 package br.com.digitalhouse.harvardartmuseums.events;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import br.com.digitalhouse.harvardartmuseums.R;
+import br.com.digitalhouse.harvardartmuseums.favs.FavsActivity;
 
 public class EventsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Events");
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.navigation_events) {
+            Intent intent = new Intent(EventsActivity.this, FavsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
