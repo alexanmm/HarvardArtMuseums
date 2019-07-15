832ae051cf2cda890779a9cf3742e2185da84f66
sergioitazar <itazar@gmail.com>
2019-07-15 19:04:12 -0300

    teste 2

--- app/src/main/java/br/com/digitalhouse/harvardartmuseums/fragments/events/EventFragment.java
+++ app/src/main/java/br/com/digitalhouse/harvardartmuseums/fragments/events/EventFragment.java
@@ -3,12 +3,13 @@ package br.com.digitalhouse.harvardartmuseums.fragments.events;
 
 import android.content.Context;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
